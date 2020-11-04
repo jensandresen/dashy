af6bff9bcc5a389e4a88c9b1ff6963d88809b359
Jens Andresen <jensandresen@gmail.com>
2020-11-04 20:49:57 +0100

    fix issue with not updating weather

--- frontend/src/widgets/current-weather/index.js
+++ frontend/src/widgets/current-weather/index.js
@@ -3,6 +3,19 @@ import { css, jsx } from "@emotion/core";
 import React, { useEffect, useState } from "react";
 import { Title, Big, PowerOf } from "components/text";
 
+import {
+  Cloudy,
+  Rainy,
+  CloudyWithLightning,
+  ClearNight,
+  CloudyWithMoon,
+  CloudyWithRainAndLightning,
+  CloudyWithSun,
+  Snowy,
+  Sunny,
+  SunnyWithWind,
+} from "./icons";
+
 function SunUpDown({ up, down }) {
   return (
     <Title>
@@ -25,9 +38,15 @@ export default function CurrentWeather() {
   const [weatherInfo, setWeatherInfo] = useState({});
 
   useEffect(() => {
-    fetch("api/weather/current")
-      .then((response) => response.json())
-      .then((data) => setWeatherInfo(data));
+    const update = () =>
+      fetch("api/weather/current")
+        .then((response) => response.json())
+        .then((data) => setWeatherInfo(data));
+
+    const handle = setInterval(update, 1000 * 60 * 2);
+    update();
+
+    return () => clearInterval(handle);
   }, []);
 
   return (
@@ -37,6 +56,17 @@ export default function CurrentWeather() {
       `}
     >
       <Temperature temperature={weatherInfo.temperature} />
+      {/* <Cloudy width="6rem" /> */}
+      {/* <Rainy width="6rem" /> */}
+      {/* <CloudyWithLightning width="6rem" />
+      <ClearNight width="6rem" />
+      <CloudyWithMoon width="6rem" />
+      <CloudyWithRainAndLightning width="6rem" />
+      <CloudyWithSun width="6rem" />
+      <Snowy width="6rem" />
+      <Sunny width="6rem" />
+      <SunnyWithWind width="6rem" /> */}
+
       {/* <SunUpDown up={weatherInfo.sunUp} down={weatherInfo.sunDown} /> */}
     </div>
   );
